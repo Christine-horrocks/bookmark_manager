3fbfe94b2e2b415fc5a25e6f800c6c12c87e09f8
Jon Sanders <jonsanders101@aol.com>
2017-10-18 13:32:35 +0100

    Connfigured Sinatra to run in development mode by default

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,7 +37,7 @@ RSpec.configure do |config|
   config.after(:each) do
     DatabaseCleaner.clean
   end
-  
+
   config.after(:suite) do
     puts
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
